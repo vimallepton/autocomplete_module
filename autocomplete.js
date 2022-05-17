35a935861ddf5c06b6147f21ed9b53fc875dfddc
vimallepton <80830156+vimallepton@users.noreply.github.com>
2022-05-17 10:22:59 +0530

    Update autocomplete.js

--- autocomplete.js
+++ autocomplete.js
@@ -66,7 +66,7 @@ function Addressinput(props) {
   }, [props.searchval]);
   (0, _react.useEffect)(() => {
     setAddress(props.label || 'Company address');
-  }, props.label);
+  }, [props.label]);
   (0, _react.useEffect)(() => {
     if (testRef.current) {
       testRef.current.scrollIntoViewIfNeeded();
@@ -93,6 +93,7 @@ function Addressinput(props) {
     }
 
     if (e.keyCode === 9) {
+      setValue(placePredictions[cursor].description);
       setShowHideList(false);
     }
 
@@ -131,7 +132,7 @@ function Addressinput(props) {
         if (line1 === '') line1 = `${fullAddress[i]}`;else line1 = `${line1},${fullAddress[i]}`;
       } else if (line2.length + fullAddress[i].length < props.limit) {
         if (line2 === '') line2 = `${fullAddress[i]}`;else line2 = `${line2},${fullAddress[i]}`;
-      } else {
+      } else if (line3.length + fullAddress[i].length < props.limit) {
         if (line3 === '') line3 = `${fullAddress[i]}`;else line3 = `${line3},${fullAddress[i]}`;
       }
     }
@@ -160,7 +161,7 @@ function Addressinput(props) {
     ref: clickRef,
     onClick: handleClickInside
   }, /*#__PURE__*/_react.default.createElement("label", {
-    className: "lable-txt"
+    className: props.labelclass ? props.labelclass : "lable-txt"
   }, address), /*#__PURE__*/_react.default.createElement("br", null), /*#__PURE__*/_react.default.createElement("input", {
     ref: focusRef,
     id: "address",
@@ -220,7 +221,7 @@ function Companyname(props) {
   const focusRef = (0, _react.useRef)();
   const inputTag = (0, _react.useRef)();
   const inactiveTestRef = (0, _react.useRef)();
-  const [value, setValue] = (0, _react.useState)("");
+  const [value, setValue] = (0, _react.useState)([]);
   const [showHideList, setShowHideList] = (0, _react.useState)(true);
   const [address, setAddress] = (0, _react.useState)("");
   const [a, setA] = (0, _react.useState)("");
@@ -229,11 +230,14 @@ function Companyname(props) {
   const [clickedOutside, setClickedOutside] = (0, _react.useState)(false);
   const [width, setWidth] = (0, _react.useState)(null);
   const [placePredictions, setPlacePredictions] = (0, _react.useState)([]);
+  const [checker, setChecker] = (0, _react.useState)('');
 
   const getPlacePredictions = data => {
-    if (data.input.length > 2) fetch(`https://api.leptonsoftware.com:9962/lepton/companyname?name=${data.input}`).then(res => res.json()).then(json => {
-      setPlacePredictions(json.data.name);
-    });
+    if (data.input.length > 2) {
+      fetch(`https://api.leptonsoftware.com:9962/lepton/companyname?name=${data.input}`).then(res => res.json()).then(json => {
+        setPlacePredictions(json.data.name);
+      });
+    }
   };
 
   (0, _react.useEffect)(() => {
@@ -241,15 +245,7 @@ function Companyname(props) {
       console.log("viiii", newAddresses);
       props.parentCallback(newAddresses);
     }
-  }, [newAddresses]); // useEffect(() => { const styles = getComputedStyle(focusRef.current); let totalsize = Number(styles.width.slice(0, -2)) + Number(styles.paddingRight.slice(0, -2)) + Number(styles.paddingLeft.slice(0, -2)); getWidth(totalsize.toString() + "px"); }, []); const getWidth = (width) => { setWidth(width); }; useEffect(() => {
-  //     if (props.searchval != '') {
-  //         setValue(props.searchval);
-  //         getPlacePredictions({ input: props.searchval });
-  //         setShowHideList(true); setA(props.searchval);
-  //         focusRef.current.focus();
-  //     }
-  // }, [props.searchval]);
-
+  }, [newAddresses]);
   (0, _react.useEffect)(() => {
     setAddress(props.label || 'Company Name');
   }, props.label);
@@ -258,6 +254,9 @@ function Companyname(props) {
       testRef.current.scrollIntoViewIfNeeded();
     }
   }, [cursor]);
+  (0, _react.useEffect)(() => {
+    if (value.length < 1) setPlacePredictions('');
+  }, [value]);
 
   const handleClickOutside = e => {
     if (!clickRef.current.contains(e.target)) {
@@ -266,29 +265,24 @@ function Companyname(props) {
   };
 
   const handleClickInside = e => {
-    if (!clickRef.current.contains(e.target)) {
-      setClickedOutside(true);
-    }
+    setClickedOutside(false);
   };
 
   const handleKeyDown = e => {
-    //outer
     if (e.keyCode === 13) {
       console.log("13", placePredictions[cursor].company_name);
       setValue(placePredictions[cursor].company_name);
       props.parentCallback(placePredictions[cursor].company_name);
-      setShowHideList(false); // setShowAutoComplete(false);
+      setShowHideList(false);
     }
 
     if (e.keyCode === 9) {
-      // console.log("9",placePredictions[cursor].company_name)
-      // setValue(placePredictions[cursor].company_name);
-      setShowHideList(false); // props.parentCallback(placePredictions[cursor].company_name);
-      // setShowAutoComplete(false);
+      setValue(placePredictions[cursor].company_name);
+      props.parentCallback(placePredictions[cursor].company_name);
+      setShowHideList(false);
     }
 
     if (e.keyCode === 27) {
-      // setShowAutoComplete(false);
       setShowHideList(false);
     }
 
@@ -299,70 +293,16 @@ function Companyname(props) {
       if (e.keyCode === 13) {}
     } else if (e.keyCode === 40 && cursor < placePredictions.length - 1) {
       setCursor(cursor + 1);
-    } // //inner
-    // if (e.keyCode === 13) {
-    //     setValue(placePredictions[cursor].description);
-    //     getplace(placePredictions[cursor].place_id, props.apikey); setShowHideList(false);
-    // }
-    // if (e.keyCode === 9) { setShowHideList(false); } if (e.keyCode === 27) { setShowHideList(false); }
-    // if (e.keyCode === 38 && cursor > 0) { if (e.keyCode === 13) { } setCursor(cursor - 1, () => { }); }
-    // else if (e.keyCode === 40 && cursor < placePredictions.length - 1) { setCursor(cursor + 1); }
-
-  };
-
-  const addresscom = place => {
-    const address = place.address_components;
-    const formattedAddress = place.formatted_address;
-    const pincode = address.find(ele => ele.types.includes("postal_code"));
-    const locality = address.find(ele => ele.types.includes("locality"));
-    let city = address.find(ele => ele.types.includes("administrative_area_level_2"));
-    const state = address.find(ele => ele.types.includes("administrative_area_level_1"));
-    const country = address.find(ele => ele.types.includes("country"));
-    city = locality ? locality : city;
-    let line1 = '';
-    let line2 = '';
-    let line3 = '';
-    const fullAddress = formattedAddress.split(',');
-    fullAddress.pop();
-    fullAddress.pop();
-    fullAddress.pop();
-
-    for (let i = 0; i < fullAddress.length; i++) {
-      if (line1.length + fullAddress[i].length < props.limit) {
-        if (line1 === '') line1 = `${fullAddress[i]}`;else line1 = `${line1},${fullAddress[i]}`;
-      } else if (line2.length + fullAddress[i].length < props.limit) {
-        if (line2 === '') line2 = `${fullAddress[i]}`;else line2 = `${line2},${fullAddress[i]}`;
-      } else {
-        if (line3 === '') line3 = `${fullAddress[i]}`;else line3 = `${line3},${fullAddress[i]}`;
-      }
     }
-
-    let addressParts = {};
-    addressParts.line_one = line1;
-    addressParts.line_two = line2;
-    addressParts.line_three = line3;
-    addressParts.city = city ? city.long_name : '';
-    addressParts.state = state ? state.long_name : '';
-    addressParts.pincode = pincode ? pincode.long_name : '';
-    addressParts.country = country ? country.long_name : '';
-    return addressParts;
-  };
-
-  const getplace = (place, apikey) => {
-    fetch(`https://api.leptonsoftware.com:9962/lepton/getplace?placeid=${place}`).then(res => res.json()).then(json => {
-      setNewAddresses({
-        adressParts: addresscom(json.data),
-        googleResponse: json.data
-      });
-    });
   };
 
   return /*#__PURE__*/_react.default.createElement("div", {
     ref: clickRef,
-    onClick: handleClickInside
+    onClick: handleClickInside,
+    onBlur: handleClickOutside
   }, /*#__PURE__*/_react.default.createElement("label", {
-    className: "lable-txt"
-  }, "     ", address), " ", /*#__PURE__*/_react.default.createElement("br", null), /*#__PURE__*/_react.default.createElement("input", {
+    className: props.labelclass ? props.labelclass : "lable-txt"
+  }, address), /*#__PURE__*/_react.default.createElement("br", null), /*#__PURE__*/_react.default.createElement("input", {
     ref: focusRef,
     id: "address",
     style: {
