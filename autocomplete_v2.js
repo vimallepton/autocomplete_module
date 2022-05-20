0cc4859e7e765a7bc17b4340578202ac2d36082a
vimallepton <80830156+vimallepton@users.noreply.github.com>
2022-05-20 14:01:14 +0530

    Update autocomplete_v2.js

--- autocomplete_v2.js
+++ autocomplete_v2.js
@@ -116,9 +116,11 @@ function Addressinput(props) {
     if (e.keyCode === 9) {
       if (!value || value.length === 0) {
         setShowHideList(false);
+      } else if (value && cursor == -1) {
+        setShowHideList(false);
       } else {
-        setValue(placePredictions[cursor + 1].description);
-        getplace(placePredictions[cursor + 1].place_id, props.apikey);
+        setValue(placePredictions[cursor].description);
+        getplace(placePredictions[cursor].place_id);
         setShowHideList(false);
       }
     }
@@ -307,9 +309,11 @@ function Companyname(props) {
     if (e.keyCode === 9) {
       if (!value || value.length === 0) {
         setShowHideList(false);
+      } else if (value && cursor == -1) {
+        setShowHideList(false);
       } else {
-        setValue(placePredictions[cursor + 1].company_name);
-        props.parentCallback(placePredictions[cursor + 1].company_name);
+        setValue(placePredictions[cursor].company_name);
+        props.parentCallback(placePredictions[cursor].company_name);
         setShowHideList(false);
       }
     }
